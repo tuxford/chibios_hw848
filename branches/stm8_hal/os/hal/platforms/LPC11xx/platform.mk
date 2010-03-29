9cbd7158d7bb9a5377ff11f31adc2ea2e89cb62a
brian360 <brian360@27425a3e-05d8-49a3-a47f-9c15f0e5edd8>
2010-03-29 19:00:38 +0000

    Create branch of trunk to work on STM8 HAL drivers

---
+++ branches/stm8_hal/os/hal/platforms/LPC11xx/platform.mk
@@ -0,0 +1,5 @@
+# List of all the LPC111x platform files.
+PLATFORMSRC = ${CHIBIOS}/os/hal/platforms/LPC11xx/hal_lld.c
+
+# Required include directories
+PLATFORMINC = ${CHIBIOS}/os/hal/platforms/LPC11xx
