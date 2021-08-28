9d3e80f605000350e89b4b76c5be36f86f2e7ea0
akscram <akscram@27425a3e-05d8-49a3-a47f-9c15f0e5edd8>
2021-08-28 11:10:37 +0000

    Create a branch for CI development

---
+++ branches/ci-test/readme.txt
@@ -0,0 +1,121 @@
+*****************************************************************************
+*** Files Organization                                                    ***
+*****************************************************************************
+
+--{root}                        - ChibiOS directory.
+  +--readme.txt                 - This file.
+  +--documentation.html         - Shortcut to the web documentation page.
+  +--license.txt                - GPL license text.
+  +--demos/                     - Demo projects, one directory per platform.
+  +--docs/                      - Documentation.
+  |  +--common/                 - Documentation common build resources.
+  |  +--hal/                    - Builders for HAL.
+  |  +--nil/                    - Builders for NIL.
+  |  +--rt/                     - Builders for RT.
+  +--ext/                       - External libraries, not part of ChibiOS.
+  +--os/                        - ChibiOS components.
+  |  +--common/                 - Shared OS modules.
+  |  |  +--abstractions/        - API emulator wrappers.
+  |  |  |  +--cmsis_os/         - CMSIS OS emulation layer for RT.
+  |  |  |  +--nasa_osal/        - NASA Operating System Abstraction Layer.
+  |  |  +--ext/                 - Vendor files used by the OS.
+  |  |  +--ports/               - RTOS ports usable by both RT and NIL.
+  |  |  +--startup/             - Startup support.
+  |  +--ex/                     - EX component.
+  |  |  +--dox/                 - EX documentation resources.
+  |  |  +--include/             - EX header files.
+  |  |  +--devices /            - EX complex drivers.
+  |  +--hal/                    - HAL component.
+  |  |  +--boards/              - HAL board support files.
+  |  |  +--dox/                 - HAL documentation resources.
+  |  |  +--include/             - HAL high level headers.
+  |  |  +--lib/                 - HAL libraries.
+  |  |  |  +--complex/          - HAL collection of complex drivers.
+  |  |  |  |  +--mfs/           - HAL managed flash storage driver.
+  |  |  |  |  +--serial_nor/    - HAL managed flash storage driver.
+  |  |  |  +--fallback/         - HAL fall back software drivers.
+  |  |  |  +--peripherals/      - HAL peripherals interfaces.
+  |  |  |  +--streams/          - HAL streams.
+  |  |  +--osal/                - HAL OSAL implementations.
+  |  |  |  +--lib/              - HAL OSAL common modules.
+  |  |  +--src/                 - HAL high level source.
+  |  |  +--ports/               - HAL ports.
+  |  |  +--templates/           - HAL driver template files.
+  |  |     +--osal/             - HAL OSAL templates.
+  |  +--oslib/                  - RTOS modules usable by both RT and NIL.
+  |  |  +--include/             - OSLIB high level headers.
+  |  |  +--src/                 - OSLIB high level source.
+  |  |  +--templates/           - OSLIB configuration template files.
+  |  +--nil/                    - NIL RTOS component.
+  |  |  +--dox/                 - NIL documentation resources.
+  |  |  +--include/             - NIL high level headers.
+  |  |  +--src/                 - NIL high level source.
+  |  |  +--templates/           - NIL configuration template files.
+  |  +--rt/                     - RT RTOS component.
+  |  |  +--dox/                 - RT documentation resources.
+  |  |  +--include/             - RT high level headers.
+  |  |  +--src/                 - RT high level source.
+  |  |  +--templates/           - RT configuration template files.
+  |  +--various/                - Various portable support files.
+  +--test/                      - Kernel test suite source code.
+  |  +--lib/                    - Portable test engine.
+  |  +--hal/                    - HAL test suites.
+  |  |  +--testbuild/           - HAL build test and MISRA check.
+  |  +--nil/                    - NIL test suites.
+  |  |  +--testbuild/           - NIL build test and MISRA check.
+  |  +--rt/                     - RT test suites.
+  |  |  +--testbuild/           - RT build test and MISRA check.
+  |  |  +--coverage/            - RT code coverage project.
+  +--testex/                    - EX integration test demos.
+  +--testhal/                   - HAL integration test demos.
+
+*****************************************************************************
+*** Releases and Change Log                                               ***
+*****************************************************************************
+
+*** Next ***
+- NEW: Made STM32H7 non-cacheable memory option also shareable.
+- NEW: EFL driver and demo for STM32F3xx.
+- NEW: New unit test subsystem under /os/test. Now it is officially
+       ChibiOS/TEST.
+- FIX: Fixed STM32 ADCv3 differences in headers (bug #1182)
+       (backported to 20.3.4).
+- FIX: Fixed DMAv1 compile fail on STM32L011 (bug #1181)
+       (backported to 20.3.4).
+- FIX: Fixed error in STM32_ADCCLK_MIN for STM32F37x/hal_lld.h (bug #1180)
+       (backported to 20.3.4).
+- FIX: Fixed direct calls to RT in STM32 RTCv2 and RTCv3 (bug #1179)
+       (backported to 20.3.4).
+- FIX: Fixed STM32G071 mcuconf.h template not showing TIM14/15/16/17
+       (bug #1178).
+- FIX: Fixed path ambiguity between STM32 USARTv2 and USARTv3 drivers
+       (bug #1177).
+- FIX: Fixed invalid DMAMUX settings in DMAv1 for some devices (bug #1176)
+       (backported to 20.3.4).
+- FIX: Fixed wrong macro in the demo STM32F7xx-SPI-ADXL355 (bug #1175) 
+       (backported to 21.6.1).
+- FIX: Fixed problem with RC initialization mask of the GPIO for the 
+       STM32L4x3 (bug #1174)(backported to 20.3.4).
+- FIX: Fixed problem with N25Q driver (bug #1173)
+       (backported to 21.6.1)(backported to 20.3.4).
+- FIX: Fixed semaphores broken when CH_CFG_USE_SEMAPHORES_PRIORITY is enabled
+       (bug #1172).
+- FIX: Fixed idle thread stack area not cleared when CH_DBG_FILL_THREADS
+       is enabled (bug #1171).
+- FIX: Fixed function chRegNextThread() broken when CH_CFG_USE_DYNAMIC
+       is disabled (bug #1170).
+- FIX: Fixed insufficient information in RT registry (bug #1169).
+- FIX: Fixed code base not compatible with -Wcast-align=strict (bug #1168).
+- FIX: Fixed wrong chconf.h for ADuCM36x demos (bug #1167)
+       (backported to 21.6.1).
+- FIX: Fixed missing constant in ADuCM36x hal_lld.c (bug #1166)
+       (backported to 21.6.1)(backported to 20.3.4).
+- FIX: Fixed missing chrfcu.c file in rt.mk (bug #1165)
+       (backported to 21.6.1).
+- FIX: Fixed invalid STM32 TIM21/TIM22 debug freeze setting (bug #1164)
+       (backported to 21.6.1)(backported to 20.3.4).
+- FIX: Fixed missing RTC APB enable on STM32G0xx (bug #1163)
+       (backported to 21.6.1)
+- FIX: Fixed I2S-related definitions missing in STM32F3xx registry (bug #1162)
+       (backported to 21.6.1)(backported to 20.3.4).
+- FIX: Fixed AVR port broken (bug #1161)(backported to 21.6.1).
---
+++ branches/ci-test/tools/workflows/make.py
@@ -0,0 +1,226 @@
+#!/usr/bin/env python
+
+"""
+This script is intended to be used to run make under supervision for
+the purpose of capturing its stdout/stderr and retcode to generate
+a JUnit XML report. This report can be used in Jenkins or any other CI.
+This report contains one test suite and as many cases as many targets
+are specified.
+
+To get help on usage, possible options and their descriptions, use
+the following command:
+
+    make.py --help
+
+An example of running this script for building a demo project:
+
+    make.py -C demos/STM32/RT-STM32WB55RG-NUCLEO68 -f Makefile all
+
+This script requires the following packages to be installed:
+
+    pip install junit-xml
+    pip install pyyaml
+    pip install requests
+"""
+
+import argparse
+import os
+import re
+import subprocess
+import sys
+import time
+from xml.etree import ElementTree
+
+import junit_xml
+import requests
+import yaml
+
+
+def get_jobserver_auth_fds():
+    """Get "jobserver" file descriptors from MAKEFLAGS."""
+    make_flags = os.environ.get('MAKEFLAGS', '')
+    jobserver_auth_flag = '--jobserver-auth='
+
+    for make_flag in make_flags.split():
+        if make_flag.startswith(jobserver_auth_flag):
+            fdr, _, fdw = make_flag[len(jobserver_auth_flag):].partition(',')
+            return (int(fdr), int(fdw))
+
+    return ()
+
+
+class SkipRules:
+    def __init__(self, filename):
+        self.filename = filename
+        self.rules = None
+
+    def match(self, path, stderr):
+        if self.rules is None:
+            self.load()
+
+        for rule in self.rules:
+            if (re.match(rule['path'], path)
+                    and re.search(rule['stderr'], stderr)):
+                return True, rule['reason']
+
+        return False, None
+
+    def load(self):
+        with open(self.filename) as fd:
+            self.rules = yaml.safe_load(fd)
+
+
+def make(args):
+    if args.skip_rules:
+        skip_rules = SkipRules(args.skip_rules)
+
+    directory = args.directory or ''
+    makefile = args.makefile or ''
+
+    path = os.path.join(directory, makefile)
+    if args.prefix:
+        assert path.startswith(args.prefix)
+        path = path[len(args.prefix):].lstrip(os.path.sep)
+
+    package = path.partition(os.path.sep)[0]
+    suite = junit_xml.TestSuite(
+        path,
+        timestamp=time.time(),
+        file=path,
+        package=package,
+    )
+
+    cmd = ['/usr/bin/env', 'make']
+    if args.directory:
+        cmd.extend(['-C', args.directory])
+    if args.makefile:
+        cmd.extend(['-f', args.makefile])
+    if args.jobs > 1:
+        cmd.extend(['-j', str(args.jobs)])
+
+    jobserver_auth_fds = get_jobserver_auth_fds()
+
+    for target in args.targets:
+        if not target:
+            continue
+
+        cmd_target = cmd + [target]
+
+        timestamp=time.time()
+        start = time.monotonic()
+        # To better control the use of resources by a subprocess make, pass
+        # jobserver auth file descriptors in the subprocess, so it will
+        # participate in the jobserver protocol and schedule its jobs
+        # accordingly.
+        ret = subprocess.run(cmd_target, pass_fds=jobserver_auth_fds,
+                             capture_output=True, text=True)
+        end = time.monotonic()
+
+        case_name = 'make ' + target
+        case_log = ' '.join(cmd_target)
+        case = junit_xml.TestCase(
+            case_name,
+            classname=path,
+            file=path,
+            log=case_log,
+            timestamp=timestamp,
+            elapsed_sec=(end - start),
+            stdout=ret.stdout,
+            stderr=ret.stderr,
+        )
+        suite.test_cases.append(case)
+
+        if ret.returncode != 0:
+            skipped, msg = skip_rules.match(path, ret.stderr)
+            if skipped:
+                case.add_skipped_info(msg)
+                continue
+
+            msg = 'Ended with non-zero exit code: {}'.format(ret.returncode)
+            case.add_failure_info(msg)
+
+    if args.result == '-':
+        suite.to_file(sys.stdout, [suite])
+    else:
+        test_result_path = os.path.join(args.result, path) + '.xml'
+        with open(test_result_path, 'w') as f:
+            suite.to_file(f, [suite])
+
+
+def check_rules(args):
+    def get_signature(content):
+        root = ElementTree.fromstring(content)
+        return (
+            root.findtext('className'),
+            root.findtext('stderr'),
+        )
+
+    if not args.skip_rules:
+        sys.stderr.write('--skip-rules is required\n')
+        sys.exit(2)
+
+    skip_rules = SkipRules(args.skip_rules)
+
+    url = args.check_rules
+    if not url.endswith('/api/xml'):
+        url += '/api/xml'
+
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        sys.stderr.write('Got unexpected response {}\n'
+                         .format(resp.status_code))
+        sys.exit(3)
+
+    path, stderr = get_signature(resp.content)
+    if not (path and stderr):
+        sys.stderr.write('Cannot find path and stderr\n')
+        sys.exit(4)
+
+    matched, msg = skip_rules.match(path, stderr)
+    if matched:
+        sys.stdout.write('Matched rule: {}\n'.format(msg))
+    else:
+        sys.stdout.write('Not matched!\n')
+        sys.exit(1)
+
+
+def main():
+    parser = argparse.ArgumentParser(description=(
+        'Execute make targets and generate JUnit results'
+    ))
+    # Make specific arguments
+    parser.add_argument('-C', '--directory', metavar='dir',
+                        help='Change directory to dir (equivalent to '
+                             '`make -C dir`)')
+    parser.add_argument('-f', '--makefile', metavar='file',
+                        help='Use file as a makefile (equivalent to '
+                             '`make -f file`)')
+    parser.add_argument('-j', '--jobs', metavar='jobs',
+                        type=int, default=1,
+                        help='Number of simultaneous jobs (equivalent to '
+                             '`make -j jobs`)')
+    # Test specific arguments
+    parser.add_argument('-r', '--result', metavar='result',
+                        default='-',
+                        help='Directory to store JUnit XML test result')
+    parser.add_argument('-p', '--prefix', metavar='prefix',
+                        help=('Prefix path which should be removed for test '
+                              'result'))
+    parser.add_argument('-s', '--skip-rules', metavar='skip-rules',
+                        help='YAML-file with skip rules')
+    parser.add_argument('-c', '--check-rules', metavar='check-rules',
+                        help='URL to a test result in Jenkins job')
+    parser.add_argument('targets', metavar='target', nargs='*',
+                        default=['all', 'clean'],
+                        help='Names of targets to run')
+
+    args = parser.parse_args()
+
+    if args.check_rules:
+        check_rules(args)
+    else:
+        make(args)
+
+
+if __name__ == '__main__':
+    main()
